30e9e13eb80cc857f6e3590351b4b3f346825c8a
Jawaharlal56 <144159413+Jawaharlal56@users.noreply.github.com>
2025-06-15 20:48:09 +0530

    sdf

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; // your global CSS
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration"; // ✅ import service worker
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-
-// ✅ Register the service worker for PWA support
-serviceWorkerRegistration.register();
